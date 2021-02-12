58112bd232937613a62a3fbb56d8187924daedd8
Eliotb26 <ecb465@nyu.edu>
2021-02-12 06:01:51 -0500

    Open link and connected to main

--- visualization.py
+++ visualization.py
@@ -2,13 +2,22 @@ from arcgis.gis import GIS
 from arcgis.geocoding import geocode
 from IPython.display import display
 import json
+import pandas as pd
+from config import *
 
-#call this on main
+
 
 def visualization(df):
     '''
-    Puts every coordinate in the data frame into a GIS. Returns a link to the GIS map
+    Using the ArcGIS REST API, this logs into the gis online account to open up the map of the 
+    designated location with the datapoints corresponding to the queries, sampled, and aggregated 
+    requests. 
+
+    Input: Df: Pandas Dataframe
+    Return: Opens a link for the ArcGIS data visualization map 
     '''
+    gis = GIS(url='https://www.arcgis.com', username='sp21_cfp286', password='LoveData123')
+
     df = df.loc[:, ['Residence_Addresses_Latitude', 'Residence_Addresses_Longitude']].dropna()
     df['Residence_Addresses_Latitude'] = pd.to_numeric(df['Residence_Addresses_Latitude'])
     df['Residence_Addresses_Longitude'] = pd.to_numeric(df['Residence_Addresses_Longitude'])
