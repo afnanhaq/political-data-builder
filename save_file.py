0cfe5204794c873ef70f782aa8bd7794a07fb4ad
Eliotb26 <ecb465@nyu.edu>
2021-02-12 05:56:06 -0500

    merge save_file

--- save_file.py
+++ save_file.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from tkinter import * 
 from tkinter import ttk 
 # import only asksaveasfile from filedialog 
@@ -7,22 +8,30 @@ from tkinter.filedialog import asksaveasfile
 
 #Save File as CSV or TAB or ShapeFile 
 #TODO Finish this and test
+def start(df):
+    root = Tk() 
+    root.geometry('200x150') 
+    root.title("File Explorer")
+    save(df)
 
-root = Tk() 
-root.geometry('200x150') 
-root.title("File Explorer")
-
-def save(): 
+def save(df): 
+    file_to_save = df.to_csv(sep=",")
     files = [('CSV Files', '*.csv'), 
              ('Shape Files', '.shp'), 
              ('TAB Files', '*.tab')] 
     file = asksaveasfile(mode='w', filetypes = files, defaultextension = ".csv") 
+    #file_to_save.to_csv(file)
 
-btn = ttk.Button(root, text = 'Save', command = lambda : save()) 
-btn.pack(side = TOP, pady = 20) 
+    btn = ttk.Button(root, text = 'Save', command = lambda : save()) 
+    btn.pack(side = TOP, pady = 20) 
 
 
-def main(): 
-
+#TODO Delete this main below, only used for testing purposes
+def main():
+    data = [['tom', 10], ['nick', 15], ['juli', 14]] 
+  
+    # Create the pandas DataFrame 
+    df = pd.DataFrame(data, columns = ['Name', 'Age'])
+    start(df)
 
 main()
