64e2b9c7c6eaa5c0821f3d45756018ded4b8dc2a
Eliotb26 <ecb465@nyu.edu>
2021-02-12 06:02:37 -0500

    Using Tkinter to save as CSV, TAB, or SHP files

--- save_file.py
+++ save_file.py
@@ -7,7 +7,7 @@ from tkinter.filedialog import asksaveasfile
 
 
 #Save File as CSV or TAB or ShapeFile 
-#TODO Finish this and test
+#TODO Finish this and test does not take in a df to save 
 def start(df):
     root = Tk() 
     root.geometry('200x150') 
@@ -35,31 +35,3 @@ def main():
     start(df)
 
 main()
-
-
-# Returns aggregated demographic information for each party
-# Argument: a sampled, numeric, demographic or merged dataframe 
-def demographic_agg(df):
-    try:
-        df['counter'] = 1
-        data = pd.crosstab(index=df['Parties_Description'], columns=[df['EthnicGroups_EthnicGroup1Desc'],df['Voters_Gender']], values=df['counter'], aggfunc='sum')
-        return data
-    except:
-        print("Sorry, not Enough Information for a Demographic Breakdown")
-
-
-# Returns aggregated information on historical voting performance for each party
-# Argument: a sampled, numeric, merged dataframe
-def merged_agg(df):
-    try:
-        criteria = [df['VotingPerformanceEvenYearGeneral'].between(0, 25), 
-                df['VotingPerformanceEvenYearGeneral'].between(26, 50), 
-                df['VotingPerformanceEvenYearGeneral'].between(51, 75), 
-                df['VotingPerformanceEvenYearGeneral'].between(76, 101)]
-        values = ['Poor', 'Below_Average', 'Above_average', 'Excellent']
-
-        df['general_voting_performance'] = np.select(criteria, values, "Unknown")        
-        data = pd.crosstab(index=df['Parties_Description'], columns=df['general_voting_performance'], values=df['counter'], aggfunc='sum')
-        return data
-    except:
-        print("Sorry, not Enough Information for a Voting History Breakdown")
